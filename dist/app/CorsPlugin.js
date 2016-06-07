ff991d02b488a7d522391a308bc0921883b6a6c2
Nicola RIZZO <nicola.n.rizzo@gmail.com>
2016-06-07 14:37:31 +0200

    Add setContainer

--- dist/app/CorsPlugin.js
+++ dist/app/CorsPlugin.js
@@ -10,6 +10,7 @@ var CorsPlugin = (function () {
                 callback(null, true);
             }
         };
+        core_1.Container.setApplicationInstance(container);
         core_1.Container.getApplicationInstance().get('app').use(cors(this.instance));
     }
     CorsPlugin.prototype.getInstance = function () {
